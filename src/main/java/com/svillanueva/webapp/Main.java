aa0add18d95935f285a0c04a4fbc7375d96ea069
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-03 08:58:42 -0300

    quitando em de la clase Main

--- src/main/java/com/svillanueva/webapp/Main.java
+++ src/main/java/com/svillanueva/webapp/Main.java
@@ -4,8 +4,6 @@ import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 
 public class Main {
-    @Inject
-    private EntityManager em;
     public static void main(String[] args) {
     }
 }

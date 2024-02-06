32bfabde2fc9bba6c43143f74df287c48625ea29
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-06 20:24:31 -0300

    Internationalization support.

--- src/main/java/com/svillanueva/webapp/configs/ProducerResources.java
+++ src/main/java/com/svillanueva/webapp/configs/ProducerResources.java
@@ -1,12 +1,18 @@
 package com.svillanueva.webapp.configs;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
 import jakarta.ws.rs.Produces;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @ApplicationScoped
 public class ProducerResources {
     @Produces
+    @RequestScoped
     public FacesContext getFacesContext() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         facesContext.getExternalContext()
@@ -14,5 +20,4 @@ public class ProducerResources {
                 .setKeepMessages(true);
         return facesContext;
     }
-
 }

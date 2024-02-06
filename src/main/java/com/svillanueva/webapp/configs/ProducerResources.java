3a474ac47c2312ad0ed6ff0356298fc8d6a8aca5
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-06 20:29:54 -0300

    REFACTORIZANDO

--- src/main/java/com/svillanueva/webapp/configs/ProducerResources.java
+++ src/main/java/com/svillanueva/webapp/configs/ProducerResources.java
@@ -3,11 +3,7 @@ package com.svillanueva.webapp.configs;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.context.FacesContext;
-import jakarta.inject.Named;
 import jakarta.ws.rs.Produces;
-
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 @ApplicationScoped
 public class ProducerResources {
